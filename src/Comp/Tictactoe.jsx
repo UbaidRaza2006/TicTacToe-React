d3a5e45354fe66a623c029a6fd1ce1831230e402
ubaidraza2006 <ubaidmemon2006@gmail.com>
2023-09-13 21:08:48 +0500

    Done

--- src/Comp/Tictactoe.jsx
+++ src/Comp/Tictactoe.jsx
@@ -64,7 +64,23 @@ export default function Game() {
     setCurrentMove(nextHistory.length - 1);
   }
 
-  
+  function jumpTo(nextMove) {
+    setCurrentMove(nextMove);
+  }
+
+  const moves = history.map((squares, move) => {
+    let description;
+    if (move > 0) {
+      description = 'Go to move #' + move;
+    } else {
+      description = 'Go to game start';
+    }
+    return (
+      <li key={move}>
+        <button onClick={() => jumpTo(move)}>{description}</button>
+      </li>
+    );
+  });
 
   return (
     <div className="game">
